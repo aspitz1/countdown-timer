8ec5b7fb6e32ef46f5bf875aa0ddd255fe8bc123
Anna Spitz <aspitzemail@gmail.com>
2022-05-06 11:17:17 -0500

    Clean up redundent code

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let form = document.getElementById('timer-form');
 
-form.addEventListener('submit', function(event){
-  event.preventDefault();
+// form.addEventListener('submit')
+
+function getStartTime(){
   let formHours = Number(document.getElementById('hours').value);
   let formMinutes = Number(document.getElementById('minutes').value);
   let formSeconds = Number(document.getElementById('seconds').value);
@@ -9,19 +10,14 @@ form.addEventListener('submit', function(event){
   let startTime = new Date();
   let startYear = startTime.getFullYear();
   let startMonth = startTime.getMonth() + 1;
-    startMonth = (startMonth >= 10)? startMonth : '0' + startMonth;
   let startDate = startTime.getDate();
-    startDate = (startDate >= 10)? startDate : Number('0' + startDate);
   let startHours = startTime.getHours();
   let startMinutes = startTime.getMinutes();
   let startSeconds = startTime.getSeconds();
 
-  let endHours = ((formHours + startHours) >= 10)?
-    formHours + startHours : '0' + (formHours + startHours);
-  let endMinutes = ((formMinutes + startMinutes) >= 10)?
-    formMinutes + startMinutes : '0' + (formMinutes + startMinutes);
-  let endSeconds = ((formSeconds + startSeconds) >= 10)?
-    formSeconds + startSeconds : '0' + (formSeconds + startSeconds);
+  let endHours = formHours + startHours;
+  let endMinutes = formMinutes + startMinutes;
+  let endSeconds = formSeconds + startSeconds;
 
   startDate = (endHours < 25)? startDate : startDate + 1;
   startDate = (startDate >= 10)? startDate : '0' + startDate;
@@ -35,10 +31,9 @@ form.addEventListener('submit', function(event){
   endSeconds = (endSeconds >= 10)? endSeconds : '0' + endSeconds;
 
   let endTime = `${startDate}/${startMonth}/${startYear} ${endHours}:${endMinutes}:${endSeconds}`
+}
 
-  let total = Date.parse(endTime) - Date.parse(startTime);
-  console.log(startSeconds);
-  console.log(endTime);
-  console.log(total);
-
-})
+// let total = Date.parse(endTime) - Date.parse(startTime);
+// console.log(startSeconds);
+// console.log(endTime);
+// console.log(total);
